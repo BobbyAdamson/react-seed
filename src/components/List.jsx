07353bf0fdd01f7bb5da737088317f2a996c8c5c
Robert Adamson <robertadamson@Roberts-MacBook-Pro.local>
2016-03-06 16:08:05 -0500

    Update: Rebuild components

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,14 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
 
-var ingredients = [{'id':1,'text':'ham'}, {'id':2,'text':'carrots'}];
-
 var List = React.createClass({
     render: function(){
-        var listItems = ingredients.map(function(item){
-            return <ListItem key={item.id} ingredients={item.text} />
-        });
-    };
+        var createItem = function(text, index){
+            return <ListItem key={index + text} text={text} />;
+        };
+
+        return (<ul>{this.props.items.map(createItem)}</ul>);
+    }
 });
+
+module.exports = List;
