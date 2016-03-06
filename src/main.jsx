4e54b1bf1414bd9144ab0667f8d7512c145aedac
Bobby Adamson <bobbyadamson1@gmail.com>
2016-03-06 16:46:53 -0500

    Adding stuff

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ListManager = require('./components/ListManager.jsx');
+
+ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
