336d2f2fb3795940363a6e5d010e4cce62ec5ecb
Bobby Adamson <bobbyadamson1@gmail.com>
2016-03-06 16:09:04 -0500

    Update: rebuild components

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
+
 var ListItem = React.createClass({
     render: function(){
-        return (
-            <li>
-                <h4>{this.props.ingredient}</h4>
-            </li>
-        );
+      return(
+          <li>
+            <h4>{this.props.text}</h4>
+          </li>
+      );
     }
 });
 
